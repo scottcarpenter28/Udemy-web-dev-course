3e94e8acce98dfd4918eb6b2b84ab12f50b115ae
scottcarpenter28 <33328166+scottcarpenter28@users.noreply.github.com>
2020-07-26 21:13:20 -0400

    Finished blog dev

--- Web dev/EJS Challenge Starting Files/app.js
+++ Web dev/EJS Challenge Starting Files/app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
+
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,10 +17,13 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
 
 app.get('/',function(req, res){
   res.render('home', {
-    homeText: homeStartingContent
+    homeText: homeStartingContent,
+    contentText: posts
   });
 });
 
@@ -37,8 +43,20 @@ app.get('/compose', function(req, res){
   res.render('compose');
 });
 
+app.get('/posts/:postName', function(req, res){
+  for(var i=0; i<posts.length; i++){
+    if(_.lowerCase(posts[i].title) ===_.lowerCase(req.params.postName))
+      res.render('post',{postTitle: posts[i].title, postContent: posts[i].content});
+  }
+});
+
 app.post('/compose', function(req, res){
-  console.log(req.body.postTitle);
+  var post ={
+    title: req.body.postTitle,
+    content: req.body.post
+  };
+    posts.push(post);
+    res.redirect('/');
 });
 
 
